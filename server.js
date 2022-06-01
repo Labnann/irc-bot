8ee9fb846134ea233e5d929b0e62aedeaab0fc9c
Glitch (glitch-hello-node) <none>
2022-06-01 19:05:15 +0000

    🐧🐫 Checkpoint ./server.js:39189368/400

--- server.js
+++ server.js
@@ -22,15 +22,7 @@ let config = [
     "server": "irc.oftc.net",
     "discordToken": "OTgxNjIwODc4MjUxMDI0NDI0.GpDMSG.Y5IL8zAKX7eXi9BH-YHAD4aKVZzOe35KakS09w",
     "channelMapping": {
-      "#981620682888736869": "#special-irc-323"
-    },
-    "ircOptions": { // Optional node-irc options
-      "floodProtection": false, // On by default
-      "floodProtectionDelay": 1000, // 500 by default
-      "port": "6697", // 6697 by default
-      "secure": true, // enable SSL, false by default
-      "sasl": true, // false by default
-  
+      "981620682888736869": "#special-irc-323"
     }
 
   }];
