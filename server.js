c5835272380bc5e20ea67a4e3060cb87e2a4575f
Glitch (glitch-hello-node) <none>
2022-06-01 18:42:06 +0000

    👨‍👨‍👦👩‍👩‍👦 Checkpoint ./start.js:39189368/145 ./package.json:39189368/4497 ./server.js:39189368/410

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express')
+import express from 'express'
+import discordIRC from 'discord-irc'
 const app = express()
 const port = process.env.PORT;
 
-const {discordIRC} = require('discord-irc');
+
 
 
 
@@ -17,8 +18,8 @@ app.listen(port, () => {
 let config = [
   // Bot 1 (minimal configuration):
   {
-    "nickname": "test2",
-    "server": "irc://irc.oftc.net:6667",
+    "nickname": "TEST",
+    "server": "irc://irc.oftc.net",
     "discordToken": "OTgxNjIwODc4MjUxMDI0NDI0.GpDMSG.Y5IL8zAKX7eXi9BH-YHAD4aKVZzOe35KakS09w",
     "channelMapping": {
       "#a-channel": "#special-irc-323"
